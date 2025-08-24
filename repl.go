24f9ba15a5fd04f959d2693ab081c2b3ba5f1be4
Aditya Dahake <dahakeadi15@gmail.com>
2025-08-24 10:17:26 +0530

    refactor: move commands to their own scripts

--- repl.go
+++ repl.go
@@ -58,22 +58,3 @@ func cleanInput(text string) []string {
 	words := strings.Fields(lowerText)
 	return words
 }
-
-func commandExit() error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
-func commandHelp() error {
-	fmt.Println("")
-	fmt.Println("Welcome to the Pokedex!")
-	fmt.Println("Usage:")
-	fmt.Println("")
-	for _, cmd := range getCommands() {
-		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
-	}
-	fmt.Println("")
-
-	return nil
-}
