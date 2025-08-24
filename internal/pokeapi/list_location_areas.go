c4a817ef64241559ee581046e720b52c391c90af
Aditya Dahake <dahakeadi15@gmail.com>
2025-08-24 14:11:06 +0530

    feat: create internal api for handling requests

---
+++ internal/pokeapi/list_location_areas.go
@@ -0,0 +1,37 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func (c *Client) ListLocationAreas(pageURL *string) (ApiRespLocationAreas, error) {
+	url := baseURL + "/location-area"
+	if pageURL != nil {
+		url = *pageURL
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return ApiRespLocationAreas{}, err
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return ApiRespLocationAreas{}, err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		return ApiRespLocationAreas{}, err
+	}
+
+	locationAreasResponse := ApiRespLocationAreas{}
+	if err := json.Unmarshal(body, &locationAreasResponse); err != nil {
+		return ApiRespLocationAreas{}, err
+	}
+
+	return locationAreasResponse, nil
+}
---
+++ internal/pokeapi/types_location_areas.go
@@ -0,0 +1,11 @@
+package pokeapi
+
+type ApiRespLocationAreas struct {
+	Count    uint    `json:"count"`
+	Next     *string `json:"next"`
+	Previous *string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
