1cc33cfa3e919432adae95c4b0e495d2762647c1
Aditya Dahake <dahakeadi15@gmail.com>
2025-08-25 10:50:08 +0530

    feat: list pokemon in an area

--- command_explore.go
+++ command_explore.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func commandExplore(cfg *config, locationArea string) error {
+	fmt.Printf("Exploring %s...\n", locationArea)
+
+	pokemonInArea, err := cfg.pokeapiClient.ListPokemonInArea(locationArea)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Found Pokemon:")
+	for _, pokemon := range pokemonInArea {
+		fmt.Println(" - ", pokemon)
+	}
+
 	return nil
 }
